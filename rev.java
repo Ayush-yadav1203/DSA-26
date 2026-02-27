43165eb8bce76937a49cb05fe9be445c6ea37d26
AYUSH YADAV <raosahab.ay1203@gmail.com>
2026-02-27 12:25:05 +0000

    reverse

---
+++ rev.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class rev{
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        int n =in.nextInt();
+        int ans=0;
+        while(n>0){
+            int rem=n%10;
+            ans=ans*10+rem;
+            n=n/10;
+            
+        }
+        System.out.println(ans);
+        
+    }
+}
